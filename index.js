39ea16ff27f81c8a85eb0ae0b83840c756074a59
Louis Antoine Otter <louis.otter@gmail.com>
2018-07-28 03:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 
 function nowServing (katzDeliLine){
 
-`Currently serving ${katzDeliLine.shift()}.`
+if (katzDeliLine.length === 0)return 'There is nobody waiting to be served!';
+return `Currently serving ${katzDeliLine.shift()}.`
 
 }
 
 function currentLine (katzDeliLine){
 
 
-  
+
 }
