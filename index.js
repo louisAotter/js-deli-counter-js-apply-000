b5e2493f6db64e955350333f6ac1e2089e267da6
Louis Antoine Otter <louis.otter@gmail.com>
2018-07-28 03:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let out = "The line is currently: ";
 if (katzDeliLine.length === 0)return 'The line is currently empty.';
 
 for (i =1; i <= katzDeliLine.length; i++){
-out = `${out}${i} .${katzDeliLine[i-1],}`
+out = `${out}${i} .${katzDeliLine[i-1]},`
 
 }
 
